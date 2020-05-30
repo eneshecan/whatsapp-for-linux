3a2239bd486325f645238ad8f41fccdd99316552
Blair Bonnett <blair.bonnett@gmail.com>
2020-05-30 14:49:57 +0200

    Add compile-time setting for the path to the UI resources.
    * Add CMake options to enable installation of the resources and their path
      (relative to the installation prefix).
    * Use CMake's configure_file() to create a config.hpp from a template making
      these settings available to the code.
    * Make the UI and icons load relative to the paths in config.hpp.
    
    The default behaviour is as before; -DINSTALL_RESOURCES=on is required
    to change the behaviour for a full install.
    
    Closes GitHub issue #14.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include "MainWindow.hpp"
+#include "Config.hpp"
 
 
 int main(int argc, char** argv)
 {
     auto const app = Gtk::Application::create(argc, argv, {});
 
-    auto const refBuilder = Gtk::Builder::create_from_file("ui/MainWindow.ui");
+    auto const refBuilder = Gtk::Builder::create_from_file(uiDir / "MainWindow.ui");
 
     MainWindow* mainWindow = nullptr;
     refBuilder->get_widget_derived("main_window", mainWindow);
