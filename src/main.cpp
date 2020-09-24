c8255cf944c532b21256215c28a92d1717738b20
Sergey Feschukov <snfesh@gmail.com>
2020-09-24 10:30:45 +0500

    Added inactive system tray icon

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include "MainWindow.hpp"
+#include "gtkmm.h"
+#include "/usr/include/libappindicator3-0.1/libappindicator/app-indicator.h"
 
 
 int main(int argc, char** argv)
@@ -12,6 +14,17 @@ int main(int argc, char** argv)
     MainWindow* mainWindow = nullptr;
     refBuilder->get_widget_derived("main_window", mainWindow);
     pMainWindow.reset(mainWindow);
+    
+    AppIndicator* indicator = 
+        app_indicator_new("whatsapp-for-linux", "whatsapp-for-linux", APP_INDICATOR_CATEGORY_HARDWARE);
+    Gtk::Menu indicatorMenu;
+    Gtk::MenuItem quit("Quit");
+ 
+    indicatorMenu.append(quit);
+ 
+    app_indicator_set_status(indicator, APP_INDICATOR_STATUS_ACTIVE);
+    app_indicator_set_menu(indicator, indicatorMenu.gobj());
+    app_indicator_set_attention_icon (indicator, "whatsapp-for-linux");
 
     return app->run(*mainWindow);
 }
