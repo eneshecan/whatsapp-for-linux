a86df2930a7422d151cd2eb7c551d29fabf301d1
Enes Hecan <nshecan@gmail.com>
2023-05-21 13:51:24 +0200

    Cancel any sound before playing

--- src/util/Sound.cpp
+++ src/util/Sound.cpp
@@ -23,8 +23,18 @@ namespace wfl::util
 
     void Sound::play(std::string_view name)
     {
-        auto ret = ca_context_play(m_context, 0U, CA_PROP_EVENT_ID, name.data(), nullptr);
-        if (ret < 0)
+        // This is simply the id we've picked which is somehow irrelevant
+        constexpr auto const id = 3U;
+
+        auto playing = 0;
+        ca_context_playing(m_context, id, &playing);
+        if (playing != CA_SUCCESS)
+        {
+            ca_context_cancel(m_context, id);
+        }
+
+        auto const ret = ca_context_play(m_context, id, CA_PROP_EVENT_ID, name.data(), nullptr);
+        if (ret != CA_SUCCESS)
         {
             std::cerr << "Sound: Failed to play " << name << ": " << ca_strerror(ret) << std::endl;
         }
