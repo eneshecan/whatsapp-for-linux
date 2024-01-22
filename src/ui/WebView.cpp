aada9eff167176765a136a207829fe7a5455d243
Enes Hecan <nshecan@gmail.com>
2024-01-23 00:51:20 +0100

    Make linter happy

--- src/ui/WebView.cpp
+++ src/ui/WebView.cpp
@@ -14,7 +14,7 @@ namespace wfl::ui
     namespace
     {
         constexpr auto const WHATSAPP_WEB_URI = "https://web.whatsapp.com";
-        constexpr auto const USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36";
+        constexpr auto const USER_AGENT       = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36";
 
         std::optional<std::string> getSystemLanguage()
         {
